5d532421b2b5358892413a272dde8c6be108619e
anestxs <crtrever@gmail.com>
2024-04-27 23:11:03 +0300

    1

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,14 @@
+import e from "cors";
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().required(),
+  email: Joi.string().required().email(),
+  phone: Joi.number().required(),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string(),
+  email: Joi.string().email(),
+  phone: Joi.number(),
+}).or("name", "email", "phone");
